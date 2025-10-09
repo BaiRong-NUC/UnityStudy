ac9e51bba8a062d8e68fb5dd5ac664ffa53ad078
BaiRong-NUC <gby@edu.nuc.email>
2025-10-09 23:27:36 +0800

    Unity 窗口,模态窗口, 可拖动窗口

--- Assets/Sctipts/18. GUI复合控件/GUICompositeControls.cs
+++ Assets/Sctipts/18. GUI复合控件/GUICompositeControls.cs
@@ -12,6 +12,10 @@ public class GUICompositeControls : MonoBehaviour
     public Rect groupRect = new Rect(10, 250, 300, 200);
 
     private Vector2 scrollPosition = Vector2.zero; // 当前滚动位置
+
+    bool showModalWindow = true; // 控制模态窗口显示与否
+
+    private Rect dragWindowRect = new Rect(200, 500, 300, 200); // 窗口位置和大小
     private void OnGUI()
     {
         // 1. 工具栏
@@ -67,7 +71,7 @@ public class GUICompositeControls : MonoBehaviour
         // 3. 分组
         // 用于批量控制空间位置,可以通过控制分组控制包裹控件的位置.可以理解为 为包裹其中的控件添加了父节点
         GUI.BeginGroup(groupRect);
-        GUI.Box(new Rect(0, 0, 100, 50), "分组BOX");
+        GUI.Box(new Rect(0, 0, 100, 200), "分组BOX");
         GUI.Label(new Rect(10, 60, 200, 30), "分组内的标签", blackStyle);
         GUI.EndGroup();
 
@@ -80,5 +84,54 @@ public class GUICompositeControls : MonoBehaviour
         // 在滚动区域内绘制内容
         this.selectionGridIndex = GUI.SelectionGrid(new Rect(0, 0, 180, 300), this.selectionGridIndex, new string[] { "选项1", "选项2", "选项3", "选项4", "选项5", "选项6" }, 1);
         GUI.EndScrollView();
+
+        // 5. 窗口
+        // 语法：Rect Window(int id, Rect clientRect, GUI.WindowFunction func, string text);
+        // 说明：id：窗口ID,每个窗口ID必须唯一；clientRect：位置和大小；func：窗口函数,用于绘制窗口内容；text：窗口标题
+        // 返回值：返回窗口位置和大小
+        GUI.Window(0, new Rect(200, 200, 300, 200), (id) =>
+       {
+           // 在这里绘制窗口内容
+           if (id == 0)
+           {
+
+               //  id==0的窗口
+               GUI.Button(new Rect(0, 0, 80, 30), "窗口按钮"); //坐标(0,0)是窗口的左上角
+           }
+       }, "窗口标题");
+
+        // 6. 模态窗口,可以让用户强制与窗口交互,直到窗口关闭
+        if (showModalWindow)
+        {
+            GUI.ModalWindow(1, new Rect(550, 200, 300, 200), (id) =>
+            {
+                // 在这里绘制窗口内容
+                if (id == 1)
+                {
+                    // id==1的窗口
+                    GUI.Label(new Rect(10, 70, 200, 30), "关闭窗口其他组件才可以响应");
+                    if (GUI.Button(new Rect(0, 30, 200, 30), "关闭模态窗口按钮")) //坐标(0,0)是窗口的左上角
+                    {
+                        // 关闭模态窗口
+                        // Debug.Log("模态窗口按钮被点击");
+                        // 注意: Unity的GUI系统没有内置关闭模态窗口的方法,通常通过设置一个布尔变量来控制模态窗口的显示与否
+                        showModalWindow = false;
+                    }
+                }
+            }, "模态窗口标题");
+        }
+
+        // 7. 拖动窗口
+        this.dragWindowRect = GUI.Window(2, this.dragWindowRect, (id) =>
+        {
+            // 在这里绘制窗口内容
+            if (id == 2)
+            {
+                // id==2的窗口
+                GUI.Label(new Rect(10, 30, 200, 30), "这是一个可拖动的窗口");
+                GUI.DragWindow(); // 使窗口可拖动
+                // GUI.DragWindow(new Rect(0, 0, 10000, 30)); // 决定窗口中哪些区域可以拖动,默认不填则默认全部区域都可以拖动
+            }
+        }, "可拖动窗口标题");
     }
 }
