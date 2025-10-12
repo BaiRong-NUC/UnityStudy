319809ba799c487a82846fef22380b492e388162
BaiRong-NUC <gby@edu.nuc.email>
2025-10-12 19:25:58 +0800

    修改预制体位置,设置了自定义输入框,拖动条组件

---
+++ Assets/Sctipts/21. GUI适配分辨率封装/MyGUI/GUIInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIInput : GUIBase
+{
+    public event UnityEngine.Events.UnityAction<string> inputEvent; // 输入内容改变事件
+
+    private string lastInputStr = ""; // 上一次的输入内容
+    protected override void DrawControlNoStyle()
+    {
+        this.guiContent.text = GUI.TextField(this.guiPos.pos, this.guiContent.text);
+        if (this.guiContent.text != lastInputStr)
+        {
+            lastInputStr = this.guiContent.text;
+            inputEvent?.Invoke(this.guiContent.text);
+        }
+    }
+
+    protected override void DrawControlUseStyle()
+    {
+        this.guiContent.text = GUI.TextField(this.guiPos.pos, this.guiContent.text, this.guiStyle);
+        if (this.guiContent.text != lastInputStr)
+        {
+            lastInputStr = this.guiContent.text;
+            inputEvent?.Invoke(this.guiContent.text);
+        }
+    }
+}
---
+++ Assets/Sctipts/21. GUI适配分辨率封装/MyGUI/GUISlider.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SliderDirection
+{
+    Horizontal,
+    Vertical
+}
+
+public class GUISlider : GUIBase
+{
+    public float minValue = 0; // 最小值
+    public float maxValue = 100; // 最大值
+    public float value = 50; // 当前值
+    public SliderDirection direction = SliderDirection.Horizontal; // 滑动方向
+    public GUIStyle thumbStyle; // 滑块样式
+
+    public event UnityEngine.Events.UnityAction<float> valueChangedEvent; // 值改变事件
+
+    float lastValue = 50; // 上一次的值
+    protected override void DrawControlNoStyle()
+    {
+        if (this.direction == SliderDirection.Horizontal)
+            this.value = GUI.HorizontalSlider(this.guiPos.pos, this.value, this.minValue, this.maxValue);
+        else
+            this.value = GUI.VerticalSlider(this.guiPos.pos, this.value, this.minValue, this.maxValue);
+
+        // 检测值变化,触发事件
+        if (this.value != lastValue)
+        {
+            lastValue = this.value;
+            valueChangedEvent?.Invoke(this.value);
+        }
+    }
+
+    protected override void DrawControlUseStyle()
+    {
+        if (this.direction == SliderDirection.Horizontal)
+            this.value = GUI.HorizontalSlider(this.guiPos.pos, this.value, this.minValue, this.maxValue, this.guiStyle, this.thumbStyle);
+        else
+            this.value = GUI.VerticalSlider(this.guiPos.pos, this.value, this.minValue, this.maxValue, this.guiStyle, this.thumbStyle);
+        // 检测值变化,触发事件
+        if (this.value != lastValue)
+        {
+            lastValue = this.value;
+            valueChangedEvent?.Invoke(this.value);
+        }
+    }
+}
