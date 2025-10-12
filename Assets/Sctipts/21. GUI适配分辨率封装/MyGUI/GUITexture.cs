e13b7c554a97df771c9b3b701b3643e80a6fe902
BaiRong-NUC <gby@edu.nuc.email>
2025-10-12 19:37:45 +0800

    GUI 自定义图片Texture组件实现

---
+++ Assets/Sctipts/21. GUI适配分辨率封装/MyGUI/GUITexture.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUITexture : GUIBase
+{
+    public ScaleMode scaleMode = ScaleMode.StretchToFill; // 缩放模式
+    protected override void DrawControlNoStyle()
+    {
+        GUI.DrawTexture(this.guiPos.pos, this.guiContent.image, this.scaleMode);
+    }
+
+    // 图片绘制没有GUIStyle,这里略过
+    protected override void DrawControlUseStyle()
+    {
+        GUI.DrawTexture(this.guiPos.pos, this.guiContent.image, this.scaleMode);
+    }
+}
