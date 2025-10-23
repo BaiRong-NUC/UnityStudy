b80592c67ed45a0c109678e9ba120402b3c47e63
BaiRong-NUC <gby@edu.nuc.email>
2025-10-23 15:20:23 +0800

    unity 向量相关API

---
+++ Assets/Scripts/27. Unity向量/UnityVector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityVector : MonoBehaviour
+{
+    void Start()
+    {
+        // 1. 三维向量 - Vector3(既可以代表坐标也可以代表方向)
+        // - 位置
+        print(this.transform.position);
+        // - 方向
+        print(this.transform.forward);
+        // 2. 两点确定一个向量
+        Vector3 pointA = new Vector3(1, 1, 1);
+        Vector3 pointB = new Vector3(4, 5, 6);
+        print(pointB - pointA); // B点指向A点的方向向量
+        // 3. 0向量,负向量
+        Vector3 zero = Vector3.zero;
+        Vector3 negative = -pointA;  //大小和pointA相等，方向相反
+        print(zero);
+        print(negative);
+        // 4. 向量的长度(模)
+        print(pointA.magnitude);
+        print(pointB.magnitude);
+        // 5. 向量的标准化(归一化)
+        Vector3 normA = pointA.normalized;
+        Vector3 normB = pointB.normalized;
+        print(normA);
+        print(normB);
+        print("normA的长度:" + normA.magnitude);
+        print("normB的长度:" + normB.magnitude);
+    }
+}
