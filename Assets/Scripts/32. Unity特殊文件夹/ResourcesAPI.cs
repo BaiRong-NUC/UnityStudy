b244d01b85885401b18cd6dd749c83c2f01578e7
BaiRong-NUC <gby@edu.nuc.email>
2025-10-28 09:19:03 +0800

    Unity Resources 文件夹API

---
+++ Assets/Scripts/32. Unity特殊文件夹/ResourcesAPI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcesAPI : MonoBehaviour
+{
+    void Start()
+    {
+        // 1. Resources动态加载:通过代码加载Resources文件夹下的资源,避免拖动的繁琐操作
+
+        // 2. 常用资源类型
+        //    2.1 预设体对象: GameObject,加载需要实例化
+        //    2.2 音效文件: AudioClip
+        //    2.3 文本文件: TextAsset
+        //    2.5 图片文件: Texture
+
+        // 3. 资源同步加载
+    }
+}
