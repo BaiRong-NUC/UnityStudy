8dd11dd65d57d8981a4573ac8e5dfb1216e87e92
BaiRong-NUC <gby@edu.nuc.email>
2025-11-03 21:55:44 +0800

    封装XML数据管理类,可以快速的实现类数据的XML持久化存储和读取功能。

---
+++ Assets/Scripts/37. XML数据持久化/TestXmlDataManage.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData
+{
+    public int id;
+    public string itemName;
+    public int quantity;
+}
+
+public class UserData
+{
+    public string userName;
+    public int level;
+    public float health;
+
+    public List<ItemData> allItems;
+
+    public ItemData[] specialItems;
+
+    public SerializationDictionary<string, int> itemQuantities;
+}
+
+public class TestXmlDataManage : MonoBehaviour
+{
+    void Start()
+    {
+        // 初始化数据类
+        UserData userData = new UserData();
+        userData.userName = "Hero";
+        userData.level = 5;
+        userData.health = 75.5f;
+        userData.allItems = new List<ItemData>()
+        {
+            new ItemData() { id = 1, itemName = "Sword", quantity = 1 },
+            new ItemData() { id = 2, itemName = "Shield", quantity = 1 },
+            new ItemData() { id = 3, itemName = "Potion", quantity = 5 }
+        };
+        userData.itemQuantities = new SerializationDictionary<string, int>()
+        {
+            { "Sword", 1 },
+            { "Shield", 1 },
+            { "Potion", 5 }
+        };
+        userData.specialItems = new ItemData[]
+        {
+            new ItemData() { id = 101, itemName = "Excalibur", quantity = 1 },
+            new ItemData() { id = 102, itemName = "Aegis Shield", quantity = 1 }
+        };
+
+        XmlDataManage.instance.SaveData(userData, "UserData.xml");
+
+        // 加载数据
+        UserData loadedData = (UserData)XmlDataManage.instance.LoadData(typeof(UserData), "UserData.xml");
+        Debug.Log("加载的用户数据: 用户名=" + loadedData.userName + ", 等级=" + loadedData.level + ", 生命值=" + loadedData.health);
+        foreach (var item in loadedData.allItems)
+        {
+            Debug.Log("物品: ID=" + item.id + ", 名称=" + item.itemName + ", 数量=" + item.quantity);
+        }
+        foreach (var kvp in loadedData.itemQuantities)
+        {
+            Debug.Log("物品数量: " + kvp.Key + " = " + kvp.Value);
+        }
+        foreach (var specialItem in loadedData.specialItems)
+        {
+            Debug.Log("特殊物品: ID=" + specialItem.id + ", 名称=" + specialItem.itemName + ", 数量=" + specialItem.quantity);
+        }
+    }
+}
---
+++ Assets/Scripts/37. XML数据持久化/XmlDataManage.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using UnityEngine;
+
+// 自定义字典序列化类
+public class SerializationDictionary<TKey, TValue> : Dictionary<TKey, TValue>, IXmlSerializable
+{
+    public XmlSchema GetSchema()
+    {
+        return null;
+    }
+
+    public void ReadXml(XmlReader reader)
+    {
+        bool wasEmpty = reader.IsEmptyElement;
+        reader.Read();
+
+        if (wasEmpty)
+            return;
+
+        // TypeDescriptor.GetConverter 支持泛型类型的字符串转换
+        var keyConverter = TypeDescriptor.GetConverter(typeof(TKey));
+        var valueConverter = TypeDescriptor.GetConverter(typeof(TValue));
+
+        while (reader.NodeType == XmlNodeType.Element)
+        {
+            string keyStr = reader.GetAttribute("key");
+            string valueStr = reader.GetAttribute("value");
+
+            TKey key = (TKey)keyConverter.ConvertFromString(keyStr);
+            TValue value = (TValue)valueConverter.ConvertFromString(valueStr);
+
+            this.Add(key, value);
+            reader.Read(); // 读到下一个节点
+        }
+        // 读到父节点的结束,将结束节点读取,避免影响之后的数据读取
+        if (reader.NodeType == XmlNodeType.EndElement)
+            reader.Read();
+    }
+
+    public void WriteXml(XmlWriter writer)
+    {
+        foreach (TKey key in this.Keys)
+        {
+            writer.WriteStartElement("Item");
+            writer.WriteAttributeString("key", key.ToString());
+            writer.WriteAttributeString("value", this[key].ToString());
+            writer.WriteEndElement();
+        }
+    }
+}
+
+public class XmlDataManage
+{
+    private static XmlDataManage _instance = new XmlDataManage();
+    public static XmlDataManage instance => _instance;
+    private XmlDataManage() { }
+
+    // 保存数据到 XML 文件
+    public void SaveData(object data, string fileName)
+    {
+        //1. 得到存储路径
+        string path = Application.persistentDataPath + "/" + fileName;
+        Debug.Log("XML数据存储路径: " + path);
+        // 2. 存储文件
+        using (StreamWriter writer = new StreamWriter(path))
+        {
+            // 3. 创建Xml序列化器
+            Type type = data.GetType();
+            System.Xml.Serialization.XmlSerializer xmlSerializer = new System.Xml.Serialization.XmlSerializer(type);
+            // 4. 序列化对象到文件
+            xmlSerializer.Serialize(writer, data);
+        }
+    }
+
+    public object LoadData(Type type, string fileName)
+    {
+        // 1. 判定文件是否存在
+        string path = Application.persistentDataPath + "/" + fileName;
+        Debug.Log("XML数据加载路径: " + path);
+        if (!File.Exists(path))
+        {
+            path = Application.streamingAssetsPath + "/" + fileName; // 从StreamingAssets目录加载默认数据
+            if (!File.Exists(path))
+            {
+                Debug.LogError("XML数据文件不存在,返回默认数据");
+                return Activator.CreateInstance(type);
+            }
+        }
+        // 2. 读取文件
+        using (StreamReader reader = new StreamReader(path))
+        {
+            // 3. 创建Xml序列化器
+            System.Xml.Serialization.XmlSerializer xmlSerializer = new System.Xml.Serialization.XmlSerializer(type);
+            // 4. 反序列化对象
+            object data = xmlSerializer.Deserialize(reader);
+            return data;
+        }
+    }
+}
