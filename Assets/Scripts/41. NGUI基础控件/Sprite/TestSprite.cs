d50ff4bd124a0d4398cd46f3a10bcf4ae11e9225
BaiRong-NUC <gby@edu.nuc.email>
2025-11-04 22:31:55 +0800

    制作登录界面UI图集,补充了Sprite修改图集方式

---
+++ Assets/Scripts/41. NGUI基础控件/Sprite/TestSprite.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSprite : MonoBehaviour
+{
+    public UISprite sprite;
+
+    void Start()
+    {
+        // 修改尺寸
+        sprite.width = 200;
+        sprite.height = 200;
+
+        // 修改图片(当前图集)
+        sprite.spriteName = "bk";
+
+        // 修改其他图集中的图片
+        NGUIAtlas atlas = Resources.Load<NGUIAtlas>("NGUI/Atlas/LogAtlas");
+        sprite.atlas = atlas;
+        sprite.spriteName = "ui_DL_liuchang_01";
+
+        // 其余属性直接访问对应成员修改即可
+    }
+}
