6f370a2ced2efc1be4a5ac1b05cbba7757663939
BaiRong-NUC <gby@edu.nuc.email>
2025-11-05 08:56:04 +0800

    NGUI Label的使用与属性

---
+++ Assets/Scripts/41. NGUI基础控件/Label/TestLabel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLabel : MonoBehaviour
+{
+    public UILabel label;
+
+    void Start()
+    {
+        this.label.text = "Hello NGUI Label";
+        this.label.color = Color.red;
+        this.label.fontSize = 36;
+        this.label.width = 400;
+        this.label.height = 100;
+    }
+}
