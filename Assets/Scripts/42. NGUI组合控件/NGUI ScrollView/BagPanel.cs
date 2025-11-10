3c2c63eb3f4ea9e41ce20355041b30a5744c0d40
BaiRong-NUC <gby@edu.nuc.email>
2025-11-10 20:12:08 +0800

    使用NGUI ScrollView 实现背包功能Demo

---
+++ Assets/Scripts/42. NGUI组合控件/NGUI ScrollView/BagPanel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BagPanel : MonoBehaviour
+{
+    private static BagPanel _instance;
+    public static BagPanel Instance => _instance;
+
+    public UIScrollView scrollView;
+
+    public UIButton closeButton;
+
+    private GameObject item;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void Start()
+    {
+        closeButton.onClick.Add(new EventDelegate(Hide));
+
+        // 创建道具图标
+        this.item = Resources.Load<GameObject>("Item");
+        // // 通过Grad自动布局
+        // for(int i = 0; i < 30; i++)
+        // {
+        //     GameObject item = Instantiate(this.item);
+        //     item.transform.SetParent(this.scrollView.transform, false);
+        // }
+        // 通过代码设置位置布局
+        for (int i = 0; i < 30; i++)
+        {
+            GameObject item = Instantiate(this.item);
+            item.transform.SetParent(this.scrollView.transform, false);
+            item.transform.localPosition = new Vector3((i % 4) * 92, -(i / 4) * 92, 0);
+
+        }
+
+        // 通过ScrollView控制滚动条更新
+        this.scrollView.UpdateScrollbars();
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
