ab41e7b9172dd9ab78a6b4a9e5d4aa7c67a4373a
BaiRong-NUC <gby@edu.nuc.email>
2025-12-05 15:15:13 +0800

    Unity 图片纹理类型属性

---
+++ Assets/Scripts/51. Unity 图片资源导入/ImportSprite.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImportSprite : MonoBehaviour
+{
+    void Start()
+    {
+        // 1. Unity支持的图片格式:
+        // BMP、JPG、PNG、PSD、TGA等
+        // 2. 图片设置的六大部分:
+        // - Texture Type: 纹理类型,如默认,Sprite(2D和UI),Cursor(光标),Cookie(灯光贴图),Advanced(高级)
+        // - Texture Shape: 纹理形状,2D或3D
+        // - Advanced: 高级设置
+        // - 平铺拉伸设置
+        // - 平台设置
+        // - 预览窗口
+    }
+}
