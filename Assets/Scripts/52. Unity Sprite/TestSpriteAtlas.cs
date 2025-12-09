7fc151b3663328fda2975fd03a94cb43eb579d03
BaiRong-NUC <gby@edu.nuc.email>
2025-12-09 14:28:55 +0800

    SpriteAtlas 精灵图集

---
+++ Assets/Scripts/52. Unity Sprite/TestSpriteAtlas.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class TestSpriteAtlas : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject gameObject = new GameObject("SpriteAtlasObject");
+        SpriteRenderer spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+        // 加载图集MySpriteAtlas是图集的名称
+        SpriteAtlas spriteAtlas = Resources.Load<SpriteAtlas>("SpriteAtlas");
+        spriteRenderer.sprite = spriteAtlas.GetSprite("dead1"); // dead1是图集中精灵的名称
+    }
+}
