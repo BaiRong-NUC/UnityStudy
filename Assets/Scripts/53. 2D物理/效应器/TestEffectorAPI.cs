e37f222bed25259a1cb7efec8efcb0a9ab3300ec
BaiRong-NUC <gby@edu.nuc.email>
2025-12-17 22:10:56 +0800

    平台xiao'yao'y

--- Assets/Scripts/53. 2D物理/效应器/TestEffectorAPI.cs
+++ Assets/Scripts/53. 2D物理/效应器/TestEffectorAPI.cs
@@ -35,5 +35,14 @@ public class TestEffectorAPI : MonoBehaviour
         //       - Constant: 恒定力，不随距离变化。
         //       - Inverse Square: 反平方力，力的大小呈反平方关系。
         //       - Inverse Linear: 反线性力，随距离反向线性变化。
+
+        // 5. 平台效应器: 用于创建单向平台效果的效应器。Is Trigger 不选中。 可以从下面穿过平台跳上去。
+        // - Rotational Offset: 平台的旋转偏移角度。
+        // - Use One Way: 是否启用单向碰撞。
+        // - Use One Way Grouping: 当平台式有多个碰撞器组合时，可以通过它们的组合来实现单向碰撞效果。(常用于平台上有多个碰撞器)
+        // - Surface Arc: 以局部坐标系下向上为中心,填写一个角度值,定义不允许通过的表面,该角度外的其他方向视为单向碰撞
+        // - Use Side Friction: 是否启用侧面摩擦力。
+        // - Use Side Bounce: 是否启用侧面弹力。
+        // - Side Arc: 侧面宽度范围。
     }
 }
