5379be7aa3b53593c813330855086b337d489097
BaiRong-NUC <gby@edu.nuc.email>
2025-12-20 16:54:35 +0800

    Unity 矩形瓦片与等距瓦片地图的使用练习

--- Assets/Scripts/55.TileMap/TestTileMap.cs
+++ Assets/Scripts/55.TileMap/TestTileMap.cs
@@ -6,7 +6,7 @@ public class TestTileMap : MonoBehaviour
 {
     void Start()
     {
-        // TileMap 瓦片地图
+        // 一. TileMap 瓦片地图
         // 1. Tilemap: 一般称之为瓦片地图或者平铺地图 是Unity2017中新增的功能,主要用于快速编辑2D游戏中的场景通过复用资源的形式提升地图多样性,工作原理就是用一张张的小图排列组合为一张大地图
         //  - 它和SpriteShape的异同
         //    共同点: 他们都是用于制作2D游戏的场景或地图的
@@ -22,14 +22,17 @@ public class TestTileMap : MonoBehaviour
         //   - Sprite: 使用Sprite的轮廓生成碰撞器形状
         //   - Grid: 基于瓦片单元格生成碰撞器形状
 
-        // TilePanel瓦片调色板窗口: 编辑好的地图,拖进场景中,激活TileMapRenderer组件即可显示地图
+        // 二. TilePanel瓦片调色板窗口: 编辑好的地图,拖进场景中,激活TileMapRenderer组件即可显示地图
         // 1. 参数: 
         // - Name: 瓦片调色板名称
-        // - Grid: 瓦片的网格布局(矩形/六边形/等距)
+        // - Grid: 瓦片的网格布局(矩形/六边形/等距(伪Z轴游戏))
         // - Hexagon Type: 六边形瓦片地图类型; Point Top: 点朝顶部的六边形; Flat Top: 面朝顶部的六边形
         // - Cell Size: 瓦片绘制到单元格的大小
 
         // 2. 使用: Hierarchy->2D Object -> Tilemap,之后在Tile Palette窗口中选择瓦片后再场景中绘制
         // Grade下面的子节点代表不同层级的地图
+        // 注意: 使用Isometric Z As Y选项,需要修改Edit->Project Settings->2D->Graphics->Transparency Sort Mode为Custom Axis,并设置Axis为(0,1,-0.26)
+        // 同时修改Tilemap Renderer组件的Mode为Individual
+        // Can change Z Position: 可以编辑瓦片z的位置,在吸取瓦片时开启可以使用快捷键+-调整z位置,同一格子不同Z可以绘制不同瓦片
     }
 }
