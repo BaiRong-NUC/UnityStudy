6ee246b0dfe5409adc6ea1acad71582a7c4dc257
BaiRong-NUC <gby@edu.nuc.email>
2025-12-24 10:42:18 +0800

    代码实现序列帧动画

---
+++ Assets/Scripts/56. Animation/序列帧动画/Animation2D.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation2D : MonoBehaviour
+{
+    public Sprite[] sprites; // 存放序列帧图片的数组
+    public float frameRate = 0.1f; // 每帧持续时间
+    private float timer = 0f; // 计时器
+
+    int nowIndex = 0; // 当前显示的图片索引
+
+    private SpriteRenderer spriteRenderer; // 用于显示图片的SpriteRenderer组件
+    void Start()
+    {
+        this.spriteRenderer = this.GetComponent<SpriteRenderer>();
+        this.spriteRenderer.sprite = sprites[0];
+    }
+
+    void Update()
+    {
+        this.timer += Time.deltaTime;
+        if (this.timer >= this.frameRate)
+        {
+            this.timer = 0f;
+            nowIndex++;
+            if (nowIndex >= sprites.Length)
+            {
+                nowIndex = 0;
+            }
+            this.spriteRenderer.sprite = sprites[nowIndex];
+        }
+    }
+}
