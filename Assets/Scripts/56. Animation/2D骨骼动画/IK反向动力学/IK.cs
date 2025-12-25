8adc0431b1c2651d8cf5ce09c3ebd5781801fe5b
BaiRong-NUC <gby@edu.nuc.email>
2025-12-25 11:14:25 +0800

    导入Unity包: IK 2D 2.2.0

---
+++ Assets/Scripts/56. Animation/2D骨骼动画/IK反向动力学/IK.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IK : MonoBehaviour
+{
+    void Start()
+    {
+        // 1. IK(反向动力学Inverse Kinematics)
+        /**
+            在骨骼动画中,构建骨骼的方式称为正向动力学,表现为子骨骼的位置根据父骨骼的位置和旋转来确定
+            而IK(反向动力学)则是通过控制子骨骼的位置来自动计算父骨骼的位置和旋转
+        */
+        // 2. IK包的引入
+        /**
+            在Package Manager 的Advanced选项中,选择Show preview packages
+            搜索2D IK,点击Install安装
+            注意: 如果在添加包是出错,需要修改Windows防火墙添加入站规则
+        */
+    }
+}
