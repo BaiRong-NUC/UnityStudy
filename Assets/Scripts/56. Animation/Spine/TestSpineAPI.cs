162bf852a67ce124732ae7b14e615bd79af0bd62
BaiRong-NUC <gby@edu.nuc.email>
2025-12-25 18:30:04 +0800

    导入Spine-Unity 运行时包

---
+++ Assets/Scripts/56. Animation/Spine/TestSpineAPI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSpineAPI : MonoBehaviour
+{
+    void Start()
+    {
+        // 1. Spine是跨平台的2D骨骼动画工具,支持Unity,Cocos等引擎,在Unity中使用Spine需要导入Spine-Unity运行时包.
+    }
+}
