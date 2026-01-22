12008ccd9e7b35726c41d17ba6d9655a874c1482
BaiRong-NUC <gby@edu.nuc.email>
2026-01-22 23:09:52 +0800

    Unity 状态机行为脚本

---
+++ Assets/Scripts/58. Animation3D/状态机行为脚本/AnimatiorBehavor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatiorBehavor : StateMachineBehaviour
+{
+    public string stateName;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (stateInfo.IsName("HumanoidIdle"))
+            Debug.Log("进入HumanoidIdle状态");
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (stateInfo.IsName("HumanoidIdle"))
+            Debug.Log("退出HumanoidIdle状态");
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (stateInfo.IsName("HumanoidIdle"))
+            Debug.Log("持续HumanoidIdle状态");
+    }
+}
