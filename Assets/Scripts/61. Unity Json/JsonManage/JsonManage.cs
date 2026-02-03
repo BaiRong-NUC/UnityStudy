a887e0615b4d4feae6e7375576baa913751b46de
BaiRong-NUC <gby@edu.nuc.email>
2026-02-03 20:06:39 +0800

    设计JsonManage单例类，封装Json的序列化和反序列化方法，方便在项目中统一使用。

---
+++ Assets/Scripts/61. Unity Json/JsonManage/JsonManage.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Json序列使用的方法
+public enum JsonType
+{
+    JsonUtility,
+    LitJson,
+}
+
+// 将类的信息序列化为Json字符串,反序列化为类对象
+public class JsonManage
+{
+    private static JsonManage _instance;
+    public static JsonManage instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new JsonManage();
+            }
+            return _instance;
+        }
+    }
+    private JsonManage() { }
+
+    public string ToJson(object t, string saveFileName = "", JsonType jsonType = JsonType.LitJson)
+    {
+        string jsonStr = "";
+        if (!string.IsNullOrEmpty(saveFileName))
+        {
+            string path = Application.persistentDataPath + "/" + saveFileName;
+            switch (jsonType)
+            {
+                case JsonType.JsonUtility:
+                    jsonStr = JsonUtility.ToJson(t);
+                    break;
+                case JsonType.LitJson:
+                    jsonStr = LitJson.JsonMapper.ToJson(t);
+                    break;
+            }
+            System.IO.File.WriteAllText(path, jsonStr);
+        }
+        return jsonStr;
+    }
+
+    public T FromJson<T>(string jsonStr = "", string loadFileName = "", JsonType jsonType = JsonType.LitJson)
+    {
+        if (!string.IsNullOrEmpty(loadFileName))
+        {
+            // 先判断默认数据文件夹是否有默认文件,没有再去持久化数据文件夹找
+            string path = Application.streamingAssetsPath + "/" + loadFileName;
+            if (!System.IO.File.Exists(path))
+            {
+                path = Application.persistentDataPath + "/" + loadFileName;
+                if (!System.IO.File.Exists(path))
+                {
+                    Debug.LogError("加载Json文件失败,路径不存在:" + path);
+                    return default(T);
+                }
+            }
+            jsonStr = System.IO.File.ReadAllText(path);
+        }
+        T t = default(T);
+        switch (jsonType)
+        {
+            case JsonType.JsonUtility:
+                t = JsonUtility.FromJson<T>(jsonStr);
+                break;
+            case JsonType.LitJson:
+                t = LitJson.JsonMapper.ToObject<T>(jsonStr);
+                break;
+        }
+        return t;
+    }
+}
---
+++ Assets/Scripts/61. Unity Json/JsonManage/TestJsonManage.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;// 标记该类可以被序列化
+
+namespace JsonManageTest
+{
+    [System.Serializable]
+    public class UserItem
+    {
+        public string itemName;
+        public int itemCount;
+    }
+    [System.Serializable]
+    public class Player
+    {
+        public string name;
+        public int age;
+        public bool isMale;
+        public List<string> skills;
+        public int[] scores;
+        public List<UserItem> itemList;
+        public Dictionary<string, UserItem> items; // JsonUtility不支持Dictionary的序列化
+        [SerializeField]
+        private int money = -1; // 私有/保护字段[SerializeField]
+        public override string ToString()
+        {
+            string itemListStr = "";
+            foreach (var item in itemList)
+            {
+                itemListStr += $"(itemName: {item.itemName}, itemCount: {item.itemCount}), ";
+            }
+            string itemsStr = "";
+            foreach (var kvp in items)
+            {
+                itemsStr += $"Key: {kvp.Key}, Value: (itemName: {kvp.Value.itemName}, itemCount: {kvp.Value.itemCount}); ";
+            }
+            return $"Name: {name}, Age: {age}, IsMale: {isMale}, Skills: [{string.Join(", ", skills)}], Scores: [{string.Join(", ", scores)}], ItemList: [{itemListStr}], Items: [{itemsStr}], Money: {money}";
+        }
+    }
+}
+
+public class TestJsonManage : MonoBehaviour
+{
+    void Start()
+    {
+        JsonManageTest.Player player = new JsonManageTest.Player();
+        player.name = "李四";
+        player.age = 20;
+        player.isMale = true;
+        player.skills = new List<string>() { "跑步", "游泳", "爬山" };
+        player.scores = new int[] { 100, 98, 95 };
+        player.itemList = new List<JsonManageTest.UserItem>() { new JsonManageTest.UserItem() { itemName = "红药水", itemCount = 10 } };
+        player.items = new Dictionary<string, JsonManageTest.UserItem>();
+        JsonManageTest.UserItem item1 = new JsonManageTest.UserItem();
+        item1.itemName = "红药水";
+        item1.itemCount = 10;
+        player.items.Add("potion_red", item1);
+        print(Application.persistentDataPath);
+        JsonManage.instance.ToJson(player, "player_litjson_useManage.json", JsonType.LitJson);
+
+        JsonManageTest.Player loadPlayer = JsonManage.instance.FromJson<JsonManageTest.Player>("", "player_litjson_useManage.json", JsonType.LitJson);
+        Debug.Log("加载的玩家信息: " + loadPlayer.ToString());
+    }
+}
